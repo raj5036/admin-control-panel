4dedc8b1f5d14c28827b0947002ddf643b89db0d
Raj <97rajmath@gmail.com>
2020-12-09 01:47:50 -0500

    Admin posts Edit done

--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -24,10 +24,10 @@
                     <tr>
                         <th scope="row">{{$post->id}}</th>
                         <td><img height="50px" src="{{$post->photo ? $post->photo->file : '/images/placeholder.gif'}}" alt=""></td>
-                        <td>{{$post->user->name}}</td>
-                        <td>{{$post->category_id}}</td>
+                        <td><a href="{{route('admin.posts.edit',$post->id)}}">{{$post->user->name}}</a></td>
+                        <td>{{$post->category ? $post->category->name : 'Uncategorized'}}</td>
                         <td>{{$post->title}}</td>
-                        <td>{{$post->body}}</td>
+                        <td>{{str_limit($post->body,7)}}</td>
                         <td>{{$post->created_at->diffForHumans()}}</td>
                         <td>{{$post->updated_at->diffForHumans()}}</td>
                     </tr>
