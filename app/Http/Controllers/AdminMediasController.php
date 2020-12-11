2475ef7504a0fdcdefc15f0152779ee0d97f41c4
Raj <97rajmath@gmail.com>
2020-12-11 11:34:57 -0500

    Delete Media is working

--- app/Http/Controllers/AdminMediasController.php
+++ app/Http/Controllers/AdminMediasController.php
@@ -24,8 +24,16 @@ class AdminMediasController extends Controller
         $file->move('images',$name);
 
         Photo::create(['file'=>$name]);
+    }
 
-        
+    public function destroy($id){
+        $photo = Photo::findOrFail($id);
+
+        unlink(public_path() . $photo->file);
+
+        $photo->delete();
+
+        return redirect('/admin/media');
     }
 
 }
--- resources/views/admin/media/index.blade.php
+++ resources/views/admin/media/index.blade.php
@@ -19,9 +19,23 @@
                         
                     
                         <tr>      
-                        <td>{{$photo->id}}</td>
-                        <td><img height="50" src="{{$photo->file}}" alt=""></td>
-                        <td>{{$photo->created_at ? $photo->created_at->diffForHumans() : "No date found!"}}</td>
+                            <td>{{$photo->id}}</td>
+                            <td><img height="50" src="{{$photo->file}}" alt=""></td>
+                            <td>{{$photo->created_at ? $photo->created_at->diffForHumans() : "No date found!"}}</td>
+                            <td>
+
+                                {!! Form::open(['method'=>'DELETE', 'action'=> ['AdminMediasController@destroy', $photo->id]]) !!}
+        
+        
+                                     <div class="form-group">
+                                         {!! Form::submit('Delete', ['class'=>'btn btn-danger']) !!}
+                                     </div>
+                                {!! Form::close() !!}
+        
+        
+        
+        
+                            </td>
                         </tr>
                     
                     @endforeach
