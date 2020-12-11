00058144d2141f5ebd4b4992b7af40c38360d37e
Raj <97rajmath@gmail.com>
2020-12-11 11:27:28 -0500

    Upload Media is working

--- app/Http/Controllers/AdminMediasController.php
+++ app/Http/Controllers/AdminMediasController.php
@@ -16,5 +16,16 @@ class AdminMediasController extends Controller
     public function create(){
 
         return view('admin.media.create');
+    
     }
+    public function store(Request $request){
+        $file=$request->file('file');
+        $name=time().$file->getClientOriginalName();
+        $file->move('images',$name);
+
+        Photo::create(['file'=>$name]);
+
+        
+    }
+
 }
